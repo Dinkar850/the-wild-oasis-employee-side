443b3c4fd571c2fcbbc183b2825a532632fc0085
Dinkar850 <guptadinkar850@gmail.com>
2025-06-13 16:30:51 +0530

    fix: login redirect

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
-import { login as loginApi } from "../../services/apiAuth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { getCurrentUser, login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading: isLoggingIn } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: async () => {
+      const user = await getCurrentUser();
+      queryClient.setQueryData(["user"], user);
       toast.success("Logged in successfully");
       navigate("/dashboard", { replace: true });
     },
